347cd63438ee4c5cf9ea7e74f5b006b7177fe702
Bernt Anker <bernt.anker@gmail.com>
2020-08-25 23:39:15 +0200

    Added custom validation error

--- src/vessel-position/pipes/vessel-position-kystverket-validator.pipe.ts
+++ src/vessel-position/pipes/vessel-position-kystverket-validator.pipe.ts
@@ -1,31 +1,47 @@
-import { Injectable, PipeTransform } from '@nestjs/common';
-import { plainToClass } from 'class-transformer'
+import {
+  Injectable,
+  PipeTransform,
+  LoggerService,
+  Logger,
+  Inject,
+} from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
 import { VesselPosition } from '../interfaces/vessel-position.interface';
 import { VesselDataProvider } from '../enums/vessel-data-provider.enum';
 import { VesselPositionKystverketDto } from '../dto/vessel-position-kystverket.dto';
 import { validateSync } from 'class-validator';
+import { InvalidVesselPositionError } from '../errors/InvalidVesselPositionData.error';
 
 @Injectable()
 export class VesselPositionKystverketValidatorPipe implements PipeTransform {
+  constructor(private logger: Logger) {}
   transform(value: any): VesselPosition {
-    value.provider = VesselDataProvider.KYSTVERKET
-    const vesselPosition: VesselPositionKystverketDto = plainToClass(VesselPositionKystverketDto, value, {
-      excludeExtraneousValues: true,
-    });
+    value.provider = VesselDataProvider.KYSTVERKET;
+    if (value.MMSI == '257075250') value.MMSI = 'koko';
+    const vesselPosition: VesselPositionKystverketDto = plainToClass(
+      VesselPositionKystverketDto,
+      value,
+      {
+        excludeExtraneousValues: true,
+      },
+    );
     try {
       this._validate(vesselPosition);
-      return vesselPosition
-    } catch(error) {
-      console.log('bad data. Skipping...')
-      return null
+      return vesselPosition;
+    } catch (error) {
+      if (error instanceof InvalidVesselPositionError) {
+        this.logger.error(error.message,null, VesselPositionKystverketValidatorPipe.name)
+        return null;
+      }
+      throw error;
     }
   }
 
   private _validate(value: any) {
     const error = validateSync(value);
     if (error.length) {
-      throw new Error('Invalid Vessel Position');
+      const {constraints} = error[0]
+      throw new InvalidVesselPositionError(constraints[Object.keys(constraints)[0]]);
     }
-  
   }
 }

cf0d002a84ef7a8b9ad1cd5eb5c4b6ad5012abb3
Bernt Anker <bernt.anker@gmail.com>
2020-08-26 21:16:40 +0200

    Swagger description

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,15 @@ import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: ['log', 'error', 'warn'],
+  });
 
   const options = new DocumentBuilder()
     .setTitle('RS-Weather API description')
-    .setDescription('Vessel marine weather tracker written by Bernt Anker (2020)')
+    .setDescription(
+      'Vessel marine weather tracker written by Bernt Anker (2020)',
+    )
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, options);
